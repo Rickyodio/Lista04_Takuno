0490a439f55e23a585f6e0002f515486ddf0c43d
Ricardo Dias <ricardodiasp2005@gmail.com>
2025-03-17 10:38:11 -0300

    Create ex2.c

---
+++ ex2.c
@@ -0,0 +1,26 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+//ex2
+
+int produtoEscalar(int A[], int B[], int n){
+    int resultado = 0;
+    for (int i = 0; i < n; i++){
+        resultado += (A[i] * B[i]);
+    }
+    
+    return resultado;
+}
+
+int main() {
+    int resultado;
+    int lista1[3] = {1,2,3};
+    int lista2[3] = {3,2,1};
+    
+    resultado = produtoEscalar(lista1, lista2, 3);
+    
+    printf("O produto escalar entre as listas eh %d", resultado);
+    
+    
+
+    return 0;
+}
